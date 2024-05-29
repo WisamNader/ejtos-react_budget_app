7f74fcf4a29c86c4d4a41f99977190d7acc4f4af
Wisam Nader <eng.wisamnader@gmail.com>
2024-05-29 15:15:11 -0400

    Ready for Task #6

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,14 +59,19 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
-                    <input
-                        required='required'
-                        type='number'
-                        id='cost'
-                        value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
-                        onChange={(event) => setCost(event.target.value)}>
-                        </input>
+                
+                
+                    <label style={{ marginLeft: '2rem' , size: 10}}>
+                    £<input
+                                required='required'
+                                type='number'
+                                id='cost'
+                                value={cost}
+                                style={{ marginLeft: '0.1rem' , size: 10}}
+                                onChange={(event) => setCost(event.target.value)}>
+                            </input>
+                    </label>
+
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,12 +2,21 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { expenses, budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
     const handleBudgetChange = (event) => {
         if (event.target.value > 20000){
             alert("Budget value cannot exceed 20,000");
             event.target.value = 20000;
+        }
+        else if (event.target.value < totalExpenses){
+            alert(`You cannot reduce the budget value lower than the spending (spending = ${totalExpenses} currently) `);
+            event.target.value = totalExpenses;
         }
         else 
             setNewBudget(event.target.value);
@@ -15,8 +24,8 @@ const Budget = () => {
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{newBudget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+    <span>Budget: £</span>
+    <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
